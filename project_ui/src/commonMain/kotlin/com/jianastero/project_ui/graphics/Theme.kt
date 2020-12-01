b9187d32c29b48d9d8313bbf96ce2709668248ed
Jian James Astrero <jianjamesastrero@gmail.com>
2020-12-02 03:24:20 +0800

    create Theme.kt

---
+++ project_ui/src/commonMain/kotlin/com/jianastero/project_ui/graphics/Theme.kt
@@ -0,0 +1,10 @@
+package com.jianastero.project_ui.graphics
+
+class Theme(
+    var primaryColor: Color = Color.CYAN,
+    var primaryDarkColor: Color = Color.BLUE,
+    var accentColor: Color = Color.YELLOW,
+    var textColor: Color = Color.BLACK,
+    var backgroundColor: Color? = null,
+    var isLightMode: Boolean = true
+)
