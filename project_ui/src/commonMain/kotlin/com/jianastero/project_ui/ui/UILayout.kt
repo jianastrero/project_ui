7dd69c09b88ce1300d3466c0cb625b78696b5f90
Jian James Astrero <jianjamesastrero@gmail.com>
2020-12-02 04:06:23 +0800

    create Text builder

--- project_ui/src/commonMain/kotlin/com/jianastero/project_ui/ui/UILayout.kt
+++ project_ui/src/commonMain/kotlin/com/jianastero/project_ui/ui/UILayout.kt
@@ -1,5 +1,13 @@
 package com.jianastero.project_ui.ui
 
 open class UILayout(
-    var child: MutableList<UI> = mutableListOf()
-) : UI()
+    var children: MutableList<UI> = mutableListOf()
+) : UI() {
+
+    fun Text(text: String, block: Text.() -> Unit): Text {
+        val item = Text(text).apply(block)
+        children.add(item)
+        return item
+    }
+
+}
