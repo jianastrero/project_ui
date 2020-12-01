9c58868479e9598b76076c31ee34b5db0a05f882
Jian James Astrero <jianjamesastrero@gmail.com>
2020-12-02 03:35:57 +0800

    create simple Text.kt

---
+++ project_ui/src/commonMain/kotlin/com/jianastero/project_ui/ui/Text.kt
@@ -0,0 +1,9 @@
+package com.jianastero.project_ui.ui
+
+import com.jianastero.project_ui.graphics.Color
+
+class Text(
+    var text: String,
+    var textSize: Float = 16f,
+    var textColor: Color? = null
+) : UI()
