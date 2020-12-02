5c9c4ceba09e7153b194bc4c71063ed27ddc56ba
Jian James Astrero <jianjamesastrero@gmail.com>
2020-12-03 02:55:12 +0800

    add constructor values for Text

--- project_ui/src/commonMain/kotlin/com/jianastero/project_ui/ui/Text.kt
+++ project_ui/src/commonMain/kotlin/com/jianastero/project_ui/ui/Text.kt
@@ -1,9 +1,16 @@
 package com.jianastero.project_ui.ui
 
+import com.jianastero.project_ui.graphics.Background
 import com.jianastero.project_ui.graphics.Color
+import com.jianastero.project_ui.graphics.Theme
+import com.jianastero.project_ui.vector.Box
 
 class Text(
     var text: String = "",
     var textSize: Float = 16f,
-    var textColor: Color? = null
-) : UI()
+    var textColor: Color? = null,
+    theme: Theme = Theme(),
+    background: Background? = null,
+    margin: Box = Box(0f),
+    padding: Box = Box(0f)
+) : UI(theme, background, margin, padding)
