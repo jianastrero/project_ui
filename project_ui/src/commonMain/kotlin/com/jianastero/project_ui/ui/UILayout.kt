b730cd25701896e4256a3ef348d6568849f42a96
Jian James Astrero <jianjamesastrero@gmail.com>
2020-12-03 02:57:32 +0800

    add constructor parameters for UILayout.kt and Text function

--- project_ui/src/commonMain/kotlin/com/jianastero/project_ui/ui/UILayout.kt
+++ project_ui/src/commonMain/kotlin/com/jianastero/project_ui/ui/UILayout.kt
@@ -1,13 +1,31 @@
 package com.jianastero.project_ui.ui
 
-open class UILayout(
-    var children: MutableList<UI> = mutableListOf()
-) : UI() {
+import com.jianastero.project_ui.graphics.Background
+import com.jianastero.project_ui.graphics.Color
+import com.jianastero.project_ui.graphics.Theme
+import com.jianastero.project_ui.vector.Box
 
-    fun Text(text: String, block: Text.() -> Unit): Text {
-        val item = Text(text).apply(block)
-        children.add(item)
-        return item
-    }
+open class UILayout(
+    theme: Theme = Theme(),
+    background: Background? = null,
+    margin: Box = Box(0f),
+    padding: Box = Box(0f),
+    var children: MutableList<UI> = mutableListOf()
+) : UI(theme, background, margin, padding) {
+
+    fun Text(
+        text: String = "",
+        textSize: Float = 16f,
+        textColor: Color? = null,
+        theme: Theme = Theme(),
+        background: Background? = null,
+        margin: Box = Box(0f),
+        padding: Box = Box(0f),
+        block: Text.() -> Unit = { }
+    ): Text = com.jianastero.project_ui.ui.Text(text, textSize, textColor, theme, background, margin, padding)
+        .apply(block)
+        .also {
+            children.add(it)
+        }
 
 }
