56eb359ee0cc975cd2cd1896ac69b71b2fee9b23
Jian James Astrero <jianjamesastrero@gmail.com>
2020-12-04 00:08:14 +0800

    Create extensions for converting UILayout to RelativeLayout

---
+++ project_ui/src/androidMain/kotlin/com/jianastero/project_ui/ui/RelativeLayout.kt
@@ -0,0 +1,12 @@
+package com.jianastero.project_ui.ui
+
+import android.widget.RelativeLayout
+import com.jianastero.project_ui.toView
+
+internal fun RelativeLayout.apply(uiLayout: UILayout): RelativeLayout = this.ui(uiLayout) { relativeLayout ->
+
+    uiLayout.children.forEach {
+        relativeLayout.addView(it.toView(relativeLayout.context))
+    }
+
+}
